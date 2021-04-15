5e686699f476a533f7e534c899288efed170a00f
守真 孫 <oscarsun72@hotmail.com>
2021-04-16 01:08:51 +0800

    temp ok

---
+++ Doc_PPt/Doc_PPt/TableDocOps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doc_PPt
+{
+    class TableDocOps
+    {
+        public TableDocOps()
+        {
+        }
+
+
+
+    }
+}
