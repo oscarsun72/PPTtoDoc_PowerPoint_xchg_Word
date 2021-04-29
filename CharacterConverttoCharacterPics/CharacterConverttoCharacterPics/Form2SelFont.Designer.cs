77fe96d1d023097143250b2ae311ac0b69f8985d
Oscar Sun（孫守真字任真） <oscarsun72@hotmail.com>
2021-04-29 13:36:09 +0800

    OK

--- CharacterConverttoCharacterPics/CharacterConverttoCharacterPics/Form2SelFont.Designer.cs
+++ CharacterConverttoCharacterPics/CharacterConverttoCharacterPics/Form2SelFont.Designer.cs
@@ -49,9 +49,9 @@ namespace CharacterConverttoCharacterPics
             this.listBox1.Font = new System.Drawing.Font("PMingLiU", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 19;
-            this.listBox1.Location = new System.Drawing.Point(277, 148);
+            this.listBox1.Location = new System.Drawing.Point(277, 205);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(416, 289);
+            this.listBox1.Size = new System.Drawing.Size(416, 232);
             this.listBox1.TabIndex = 1;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
